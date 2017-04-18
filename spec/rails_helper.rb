3fa541bcf32a55263f052be3ec56a20ac4fd563e
Andres <x3enos@gmail.com>
2017-04-18 16:28:50 +0300

    ETB-13: added ability for removing categories

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,8 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'support/factory_girl'
 require 'spec_helper'
+require 'telegram/bot/rspec/integration'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -56,6 +58,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.after { Telegram.bot.reset }
 end
 
 Shoulda::Matchers.configure do |config|
---
+++ spec/requests/telegram_controller_spec.rb
@@ -0,0 +1,57 @@
+RSpec.describe TelegramController, :telegram_bot do
+  let(:user) { create(:user) }
+  before do
+    allow_any_instance_of(UserInitService).to receive(:perform) { user }
+  end
+
+  describe '#deletecategory' do
+    context "user passed the name of existing category" do
+      let(:category) { create(:category,:income, user: user) }
+      let(:category_name) { category.name }
+
+      it 'should return text about confirm the removing category' do
+        text = "Type in 'yes' if you really want to delete that category."
+        expect { dispatch_command :deletecategory, category_name }.to respond_with_message(text)
+      end
+
+      context "user passed the word 'yes'" do
+        before do
+          dispatch_command :deletecategory, category_name
+        end
+
+        it 'should delete category' do
+          expect { dispatch_message 'yes' }.to change(user.categories, :count).from(1).to(0)
+        end
+
+        it 'should return positive response' do
+          text = 'Category was deleted!'
+          expect { dispatch_message 'yes' }.to respond_with_message(text)
+        end
+      end
+
+      context "user didn't passed the word 'yes'" do
+        before do
+          dispatch_command :deletecategory, category_name
+        end
+
+        it "shouldn't delete category" do
+          expect { dispatch_message 'no' }.not_to change(user.categories, :count).from(1)
+        end
+
+        it 'should return positive response' do
+          text = 'The deleting of category was cancelled.'
+          expect { dispatch_message 'no' }.to respond_with_message(text)
+        end
+      end
+    end
+
+    context "user passed the invalid name" do
+      let(:category_name) { 'name' }
+
+      it 'should return text about confirm the removing category' do
+        text = "Unfortunately we didn't find the category with that name."
+        expect { dispatch_command :deletecategory, category_name }.to respond_with_message(text)
+      end
+    end
+  end
+end
